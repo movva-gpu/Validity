5bbb6a7df6c565e00c740e60cb734b7270e320a5
Allenyade <81530181+movva-gpu@users.noreply.github.com>
2023-11-02 08:45:34 +0100

    Undoing last commit

--- src/classes/Command.ts
+++ src/classes/Command.ts
@@ -2,5 +2,5 @@ import { ChatInputCommandInteraction, SlashCommandBuilder } from 'discord.js'
 
 export class Command {
     data = new SlashCommandBuilder().setName('').setDescription('').setDescriptionLocalization('fr', '');
-    public execute(interaction: ChatInputCommandInteraction, ...args: any): any { ... }
+    public execute(interaction: ChatInputCommandInteraction, ...args: any): any { return; }
 }
