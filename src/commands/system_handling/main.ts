aad737956c79f1f4b33c11348594677ac37f24b1
Allenyade <81530181+movva-gpu@users.noreply.github.com>
2023-11-07 00:29:09 +0100

    feat: Added /system create subcommand
    Added data/* to .gitignore too

--- src/commands/system_handling/main.ts
+++ src/commands/system_handling/main.ts
@@ -12,14 +12,66 @@ export const data = new SlashCommandBuilder()
     .addSubcommand(new SlashCommandSubcommandBuilder()
         .setName('help')
         .setDescription(enUsJson.commands.system.subcommands.help.description)
-        .setDescriptionLocalizations({ 'fr': frJson.commands.system.subcommands.help.description }));
-export function execute(interaction: ChatInputCommandInteraction) {
+        .setDescriptionLocalizations({ 'fr': frJson.commands.system.subcommands.help.description }))
+    .addSubcommand(create.data);
+export async function execute(interaction: ChatInputCommandInteraction) {
     const subCommand = interaction.options.getSubcommand(false);
     if (subCommand != '') {
         switch (subCommand) {
             case 'help':
                     invokeHelpEmbed(1, interaction);
                 break;
+            case 'create':
+                const error = await create.execute(interaction);
+                switch (error) {
+                    case create.InteractionReplyError.NoError:
+                        interaction.reply('The system was successfuly created! :)');
+                        break;
+
+                    case create.InteractionReplyError.AlreadyExists:
+                        interaction.reply('Error: It seems you already have a system :/');
+                        break;
+                    
+                    case create.InteractionReplyError.NameIsTooLong:
+                        interaction.reply('Error: Heyy! The maximum length for your system\'s name is 64 characters! >:0');
+                        break;
+                        
+                    case create.InteractionReplyError.NotHexColor:
+                        interaction.reply('Error: The color must begin with # and contain 3 or 6 characters! :3');
+                        break;
+                        
+                    case create.InteractionReplyError.DescIsTooLong:
+                        interaction.reply('Error: Wow! The description you provided is way to looong :0 The maximum size is of 1024 characters! ^^\'');
+                        break;
+
+                    case create.InteractionReplyError.BothUrlAndAttachment:
+                        interaction.reply('Error: @_@ Attachment or URL? Attachment or URL?? I can only be provided one!');
+                        break;
+                    
+                    case create.InteractionReplyError.AvatarUrl404:
+                        interaction.reply('Error 404: I can\'t access the image your provided me! Maybe you made a typo?');
+                        break;
+                        
+                    case create.InteractionReplyError.AvatarUrlIsBroken:
+                        interaction.reply('Error: You link seems to be unusable! Maybe you made a typo?');
+                        break;
+
+                    case create.InteractionReplyError.AvatarAttachmentWrongType:
+                        interaction.reply('Error: The file you provided me isn\'t an image!');
+                        break;
+
+                    case create.InteractionReplyError.AvatarAttachment404:
+                        interaction.reply('Error: Something seems to be wrong with the attachment! Try again later :p');
+                        break;
+                    
+                    case create.InteractionReplyError.AvatarAttachmentIsBroken:
+                        interaction.reply('Error: Something seems to be wrong with the attachment! Try again later :p');
+                        break;
+                        
+                    case create.InteractionReplyError.SavingError:
+                        interaction.reply('Error: Error while saving your file to the database, please [report the issue on github](https://github.com/movva-gpu/ValidityRE/issues/new?title=[DATABASE%20SAVING]&body=(Please%20put%20the%20command%20you%20input%20here))!');
+                        break;
+                }
         }
     }
 }
---
+++ src/commands/system_handling/subcommands/create.ts
@@ -0,0 +1,118 @@
+import { ChatInputCommandInteraction, SlashCommandSubcommandBuilder } from 'discord.js'
+import enUsJson from '../../../../res/en-us.json'
+import frJson from '../../../../res/fr.json'
+import { System } from '../../../classes/systemHandling/System'
+import { generateUID, getUrlResponse, saveDatabase, stringOptionNormalize } from '../../../globalMethods'
+
+export const data = new SlashCommandSubcommandBuilder()
+    .setName('create')
+    .setDescription(enUsJson.commands.system.subcommands.create.description)
+    .setDescriptionLocalizations({ fr: frJson.commands.system.subcommands.create.description })
+    .addStringOption((option) => option
+        .setName('name')
+        .setRequired(true)
+        .setDescription(enUsJson.commands.system.subcommands.create.optionsDesc.name)
+        .setDescriptionLocalizations({ fr: frJson.commands.system.subcommands.create.optionsDesc.name }))
+    .addStringOption((option) => option
+        .setName('color')
+        .setDescription(enUsJson.commands.system.subcommands.create.optionsDesc.color)
+        .setDescriptionLocalizations({ fr: frJson.commands.system.subcommands.create.optionsDesc.color }))
+    .addStringOption((option) => option
+        .setName('description')
+        .setDescription(enUsJson.commands.system.subcommands.create.optionsDesc.description)
+        .setDescriptionLocalizations({ fr: frJson.commands.system.subcommands.create.optionsDesc.description }))
+    .addStringOption((option) => option
+        .setName('avatar-url')
+        .setDescription(enUsJson.commands.system.subcommands.create.optionsDesc.avatarUrl)
+        .setDescriptionLocalizations({ fr: frJson.commands.system.subcommands.create.optionsDesc.avatarUrl }))
+    .addAttachmentOption((option) => option
+        .setName('avatar-attachment')
+        .setDescription(enUsJson.commands.system.subcommands.create.optionsDesc.avatarAttachment)
+        .setDescriptionLocalizations({ fr: frJson.commands.system.subcommands.create.optionsDesc.avatarAttachment }));
+
+export async function execute(interaction: ChatInputCommandInteraction): Promise<InteractionReplyError> {
+    const systemsData = require('../../../../data/data.json');
+    var interactionReplyErrors = [] as InteractionReplyError[];
+    var avatarUrl: string | undefined;
+
+    systemsData.systems.forEach((element: System) => {
+        const userIDs = element.userIDs;
+        for(let i = 0; i < userIDs.length; i++) {            
+            if (userIDs[i] == interaction.user.id) interactionReplyErrors.push(InteractionReplyError.AlreadyExists);
+        }
+    });
+    const nameOption = stringOptionNormalize(interaction, 'name', true) as string;
+    const colorOption = stringOptionNormalize(interaction, 'color');
+    const avatarUrlOption = stringOptionNormalize(interaction, 'avatar-url');
+    const avatarAttachmentOption = interaction.options.getAttachment('avatar-attachment', false);
+    const descOption = stringOptionNormalize(interaction, 'description');
+    
+    if (nameOption.length > 64) interactionReplyErrors.push(InteractionReplyError.NameIsTooLong)
+
+    if (colorOption) {
+        if (!/^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/.test(colorOption)) interactionReplyErrors.push(InteractionReplyError.NotHexColor);
+    }
+
+    if (descOption) {
+        if (descOption.length > 1024) interactionReplyErrors.push(InteractionReplyError.DescIsTooLong);
+    }
+
+    if (avatarUrlOption != null && avatarAttachmentOption != undefined) interactionReplyErrors.push(InteractionReplyError.BothUrlAndAttachment);
+    if (avatarAttachmentOption) {
+        if (!avatarAttachmentOption.contentType || !avatarAttachmentOption.contentType?.startsWith('image/')) interactionReplyErrors.push(InteractionReplyError.AvatarAttachmentWrongType);
+            interactionReplyErrors.push(await getUrlResponse(avatarAttachmentOption.url, InteractionReplyError.AvatarAttachment404, InteractionReplyError.AvatarAttachmentIsBroken));
+            avatarUrl = avatarAttachmentOption.url;
+    }
+
+    if (avatarUrlOption) {
+        interactionReplyErrors.push(await getUrlResponse(avatarUrlOption, InteractionReplyError.AvatarUrl404, InteractionReplyError.AvatarUrlIsBroken));
+        avatarUrl = avatarUrlOption;
+    }
+
+    for(let i = 0; i < interactionReplyErrors.length; i++) {
+        if (!interactionReplyErrors[i]) {
+            interactionReplyErrors.splice(i, 1);
+        }
+    }
+
+    if (interactionReplyErrors.length === 0) {
+        let system = new System(generateUID(), [ interaction.user.id ], colorOption, avatarUrl, undefined, nameOption, descOption);
+        const newSystemData: any = systemsData;
+        (newSystemData.systems as any[]).push(system.toJson());
+        let savingResult = saveDatabase(JSON.stringify(newSystemData))
+        interactionReplyErrors.push(savingResult);
+    }
+    console.log(interactionReplyErrors);
+    return interactionReplyErrors[0];
+}
+
+
+export enum InteractionReplyError {
+
+    NoError,
+
+
+
+    AlreadyExists,
+
+
+    
+    NameIsTooLong,
+    NotHexColor,
+    DescIsTooLong,
+
+
+
+    BothUrlAndAttachment,
+
+    AvatarUrl404,
+    AvatarUrlIsBroken,
+
+    AvatarAttachmentWrongType,
+    AvatarAttachment404,
+    AvatarAttachmentIsBroken ,
+
+
+
+    SavingError
+}
